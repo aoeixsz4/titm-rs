7c676d788681d621624f8197ffc466698ad41778
Joanna Doyle <jjadoyle@gmail.com>
2020-11-13 12:22:07 +0100

    Add some changes that don't work
    The termion library may have what we need to make this work.
    However, it doesn't work together very well with tokio...
    
    The raw stdout handle is blocking, and seemingly cannot be cloned. This
    means can't do tokio::task::spawn_blocking().
    
    Signed-off-by: Joanna Doyle <jjadoyle@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use tokio::io::{self, BufReader, AsyncReadExt, AsyncWriteExt};
-use tokio::process::{Command, ChildStdin};
+extern crate tokio;
+extern crate termion;
+extern crate log;
+use log::{debug, warn};
+use tokio::io::{BufReader, AsyncReadExt, AsyncWriteExt};
+use tokio::process::{Command, ChildStdin, ChildStdout};
 use tokio::sync::mpsc::{Sender, Receiver, channel};
 use tokio::task;
 use std::process::Stdio;
-use std::io::{stdin, Stdin};
-use std::io::Read as stdRead;
+use std::io::{Read, Write, Stdout, stdout};
+use std::sync::Arc;
+use termion::async_stdin;
+use termion::raw::{RawTerminal, IntoRawMode};
+use termion::AsyncReader as TermReader;
 
 // we'll want a spawned task with an mpsc receiver
 // user input will be sent across the channel, and the
@@ -34,15 +41,71 @@ async fn game_input_handler(mut rx: Receiver<u8>, mut game_stdin: ChildStdin) {
                 if let Ok(n_bytes) = game_stdin.write(&buffer[write_cursor .. read_index]).await {
                     write_cursor += n_bytes;
                 } else {
+                    warn!("write to child's stdin failed");
                     return;
                 }
             }
             if let Err(_) = game_stdin.flush().await {
+                warn!("flush child's stdin failed");
                 return;
             }
         } else {
+            warn!("receiving from channel failed");
             return;
         }
+    }
+}
+
+fn copy_buf(buffer: &[u8], length: usize) -> Vec<u8> {
+    let mut result = Vec::new();
+    let mut index = 0;
+    while index < length {
+        result.push(buffer[index]);
+        index += 1;
+    }
+    result
+}
+
+// handler for output to terminal
+async fn game_output_handler(mut game_stdout: ChildStdout, raw_stdout: Arc<RawTerminal<Stdout>>) {
+    let mut buffer: [u8; 4096] = [0; 4096];
+    let mut reader = BufReader::new(game_stdout);
+
+    while let Ok(n_read) = reader.read(&mut buffer).await {
+        if n_read == 0 { // EOF
+            warn!("got EOF on child's output");
+            return;
+        }
+        let vec_copy = copy_buf(&buffer, n_read);
+        let stdout_clone = Arc::clone(&raw_stdout);
+        task::spawn_blocking(move || {
+            stdout_clone.write(&vec_copy);
+            stdout_clone.flush();
+        });
+    }
+}
+
+async fn user_input_handler(mut term_input: TermReader, tx: Sender<u8>) {
+    let mut buffer: [u8; 4096] = [0; 4096];
+
+    loop {
+        // this may be a bit of a busy loop if we don't
+        // occasionally yield - apparently yielding will put
+        // us at the back of the queue but we don't necessarily
+        // need special waking
+        if let Ok(bytes_read) = term_input.read(&mut buffer) {
+            if bytes_read == 0 { warn!("EOF on term input"); continue; } // EOF
+            let mut index = 0;
+            while index < bytes_read {
+                if let Err(_) = tx.send(buffer[index]).await {
+                    warn!("send to channel failed");
+                    return;
+                } else {
+                    index += 1;
+                }
+            }
+        }
+        task::yield_now().await;
     }
 }
 
@@ -58,7 +121,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // the terminal if this process is invoked from the command line).
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::piped());
-    //cmd.stdin(Stdio::piped());
+    cmd.stdin(Stdio::piped());
 
     let mut child = cmd.spawn()
         .expect("failed to spawn command");
@@ -68,17 +131,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("child did not have a handle to stdout");
     //let mut child_stderr = child.stderr.take()
     //    .expect("child did not have a handle to stderr");
-    //let child_stdin = child.stdin.take()
-    //    .expect("child did not have a handle to stderr");
+    let child_stdin = child.stdin.take()
+        .expect("child did not have a handle to stderr");
+
+    // in order to disable input line-buffering, we need
+    // to enable raw-mode on stdout
+    // this probably means we have to use a load of
+    // spawn_blocking() calls for sending output to the screen :/
+    // may also be possible to use a blocking thread for all I/O
+    // going towards the output... will think about this
+    let mut stdout = Arc::new(stdout().into_raw_mode().unwrap());
 
     // set up channel for handler of input TO game
-    //let (tx, rx) = channel(100);
-    //task::spawn(game_input_handler(rx, child_stdin));
+    let (tx, rx) = channel(100);
+    task::spawn(game_input_handler(rx, child_stdin));
 
-    // the 'main' task simply reads data from the child's standard output,
-    // relays it to our parent stdout *and* our game watcher task
-    let mut reader = BufReader::new(child_stdout);
-    let mut buffer: [u8; 4096] = [0; 4096];
+    // set up handler for terminal input from user
+    let mut term_stdin = async_stdin();
+    task::spawn(user_input_handler(term_stdin, tx));
+
+    // set up handler for terminal output, forwarded
+    // from game stdout to the main terminal
+    task::spawn(game_output_handler(child_stdout, stdout));
 
     // Ensure the child process is spawned in the runtime so it can
     // make progress on its own while we await for any output.
@@ -88,16 +162,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("child status was: {}", status);
     });
-
-    let mut stdout = io::stdout();
-    while let length = reader.read(&mut buffer).await? {
-        if length == 0 { break } // EOF
-        let mut cursor = 0;
-        while cursor < length {
-            cursor += stdout.write(&buffer[cursor .. length]).await?;
-        }
-        stdout.flush().await?;
-    }
 
     Ok(())
 }
