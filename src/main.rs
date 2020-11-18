13028eeca66480894542ddad04a5125382494f8c
Joanna Doyle <jjadoyle@gmail.com>
2020-11-18 08:41:49 +0100

    Implement virtual terminal buffer with ANSI codes
    Using nix::pty lib and termion, the application creates a pty in which
    to run the child application (e.g. local nethack, or ssh/dgl/nethack).
    Keyboard input on the original pty is relayed to the new pty.
    Raw output from the game is also forwarded to the real terminal.
    However, a virtual terminal buffer is maintained and managed in memory
    by the program.
    This game screen will be used to investigate the inventory and game
    stats, and also possibly to automate startscumming.
    
    Signed-off-by: Joanna Doyle <jjadoyle@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ extern crate nix;
 extern crate termion;
 
 use std::error;
-use std::io::{Read, Write, stdin, stdout};
-use std::process::{Command, Stdio};
+use std::io::{Write, stdin, stdout, stderr, Stderr};
+use std::process::Command;
 use std::thread;
 use std::time::Duration;
 use nix::unistd;
@@ -34,6 +34,447 @@ use termion::{terminal_size, terminal_size_pixels};
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
+#[derive(Clone)]
+struct Position {
+    x: usize,
+    y: usize
+}
+
+#[derive(Clone)]
+enum CharMap {
+    IsoStandard,
+    UTF8,
+    VTGraphics,
+    Null,
+    User
+}
+
+
+struct Size {
+    width: usize,
+    height: usize
+}
+
+
+#[derive(Clone)]
+pub enum CSISubType {
+    Parameters,
+    Intermediates
+}
+
+#[derive(Clone)]
+pub enum EscType {
+    Simple,
+    CSI(CSISubType),
+    OSC,
+    EsqOSC
+}
+
+#[derive(Clone)]
+pub enum EntryMode {
+    Normal,
+    UTF8(usize),
+    Escape(EscType)
+}
+
+struct ScreenState {
+    cursor: Position,
+    charset: CharMap,
+    mapg0: CharMap,
+    mapg1: CharMap
+}
+
+struct GameScreen {
+    cursor: Position,
+    size: Size,
+    charset: CharMap,
+    mapg0: CharMap,
+    mapg1: CharMap,
+    mode: EntryMode,
+    escape: Vec<u8>,
+    grid: Vec<Vec<char>>,
+    savestate: ScreenState
+}
+
+
+
+fn convert_vt(text: char) -> char {
+    match text {
+        'a' => '▒',
+        'j' => '┘',
+        'k' => '┐',
+        'l' => '┌',
+        'm' => '└',
+        'n' => '┼',
+        'q' => '─',
+        't' => '├',
+        'u' => '┤',
+        'v' => '┴',
+        'w' => '┬',
+        'x' => '│',
+        '~' => '·',
+        _ => text
+    }
+}
+
+impl GameScreen {
+    pub fn new(width: usize, height: usize) -> Self {
+        let grid = vec![vec![0 as char; width]; height];
+        let cursor = Position {
+            x: 0,
+            y: 0
+        };
+        let size = Size {
+            width: width,
+            height: height
+        };
+
+        GameScreen {
+            cursor,
+            size,
+            mode: EntryMode::Normal,
+            escape: Vec::new(),
+            charset: CharMap::IsoStandard,
+            mapg0: CharMap::IsoStandard,
+            mapg1: CharMap::IsoStandard,
+            grid,
+            savestate: ScreenState {
+                cursor: Position { x: 0, y: 0 },
+                charset: CharMap::IsoStandard,
+                mapg0: CharMap::IsoStandard,
+                mapg1: CharMap::IsoStandard
+            }
+        }
+    }
+
+    pub fn update(&mut self, raw_bytes: &[u8]) {
+        for byte in raw_bytes.iter() {
+            match (self.mode.clone(), byte) {
+                (EntryMode::Normal, 0x0a) => self.line_feed(),
+                (EntryMode::Normal, 0x0d) => self.carriage_return(),
+                (EntryMode::Normal, 0x08) => self.backspace(),
+                (EntryMode::Normal, 0x1b) => {
+                    self.escape.push(*byte);
+                    self.mode = EntryMode::Escape(EscType::Simple);
+                },
+                (EntryMode::Normal, _) => self.put_char(*byte as char),
+
+                (EntryMode::Escape(_), _) => self.put_esc_code(*byte),
+                (_, _) => ()
+            }
+        }
+    }
+
+    fn put_esc_code(&mut self, byte: u8) {
+        if let EntryMode::Escape(esc_type) = self.mode.clone() {
+            self.escape.push(byte);
+
+            match (esc_type, byte) {
+                // these codes aborb an escape sequence entirely
+                (_, 0x18) => self.abort_escape(),
+                (_, 0x1a) => self.abort_escape(),
+
+                (EscType::Simple, b'[')
+                    => self.mode = EntryMode::Escape(EscType::CSI(CSISubType::Parameters)),
+                (EscType::Simple, b']')
+                    => self.mode = EntryMode::Escape(EscType::OSC),
+                
+                // for simple escapes, 0x20 - 0x2f are intermediate, and 0x30 - 0x7e are final
+                (EscType::Simple, 0x30 ..= 0x7e)
+                    => self.process_escape(),
+
+                (EscType::CSI(_subtype), 0x30 ..= 0x3f)
+                    => self.mode = EntryMode::Escape(EscType::CSI(CSISubType::Parameters)),
+                (EscType::CSI(_subtype), 0x20 ..= 0x2f)
+                    => self.mode = EntryMode::Escape(EscType::CSI(CSISubType::Intermediates)),
+                (EscType::CSI(_subtype), 0x40 ..= 0x7e) =>
+                    self.process_escape(),
+
+                (EscType::OSC, 0x1b)
+                    => self.mode = EntryMode::Escape(EscType::EsqOSC),
+                (EscType::EsqOSC, b'\\')
+                    => self.process_escape(),
+
+                (_, _) => ()
+            }
+        }
+    }
+
+    fn abort_escape(&mut self) {
+        self.mode = EntryMode::Normal;
+        self.escape.clear();
+    }
+
+    fn process_escape(&mut self) {
+        if let EntryMode::Escape(esc_type) = self.mode.clone() {
+            match esc_type {
+                EscType::Simple => self.simple_escape(),
+                EscType::CSI(_subtype) => self.csi_escape(),
+                EscType::OSC => (),
+                EscType::EsqOSC => (), //self.osc_escape()
+            }
+        }
+        self.mode = EntryMode::Normal;
+        self.escape.clear();
+    }
+
+    fn simple_escape(&mut self) {
+        assert_eq!(self.escape.remove(0), 0x1b);
+        if self.escape.is_empty() {
+            return; // should error here probably
+        }
+        match self.escape.remove(0) {
+            b'c' => (), // RIS reset
+            b'D' => (), // IND linefeed
+            b'E' => (), // NEL newline
+            b'H' => (), // HTS set tab stop at current column
+            b'M' => (), // RI reverse linefeed
+            b'Z' => (), // no idea
+            // save state (cursor position, 'attributes', and G0/G1 charsets)
+            b'7' => {
+                self.savestate.cursor = self.cursor.clone();
+                self.savestate.charset = self.charset.clone();
+                self.savestate.mapg0 = self.mapg0.clone();
+                self.savestate.mapg1 = self.mapg1.clone();
+            },
+            // restore saved state (cursor position, 'attributes', and G0/G1 charsets)
+            b'8' => {
+                self.cursor = self.savestate.cursor.clone();
+                self.charset = self.savestate.charset.clone();
+                self.mapg0 = self.savestate.mapg0.clone();
+                self.mapg1 = self.savestate.mapg1.clone();
+            },
+            //b'[' => (), begin CSI sequence, handled elsewhere
+            b'%' if ! self.escape.is_empty() => {
+                // choose character set
+                match self.escape.remove(0) {
+                    b'@' => self.charset = CharMap::IsoStandard,
+                    b'G' | b'8' => self.charset = CharMap::UTF8,
+                    _ => ()
+                }
+            },
+            b'#' => (), // DECALN screen alignment test
+            b'(' if ! self.escape.is_empty() => {
+                match self.escape.remove(0) {
+                    b'B' => self.mapg0 = CharMap::IsoStandard,
+                    b'0' => self.mapg0 = CharMap::VTGraphics,
+                    b'U' => self.mapg0 = CharMap::Null,
+                    b'K' => self.mapg0 = CharMap::User,
+                    _ => ()
+                }
+            },
+            b')' if ! self.escape.is_empty() => {
+                match self.escape.remove(0) {
+                    b'B' => self.mapg1 = CharMap::IsoStandard,
+                    b'0' => self.mapg1 = CharMap::VTGraphics,
+                    b'U' => self.mapg1 = CharMap::Null,
+                    b'K' => self.mapg1 = CharMap::User,
+                    _ => ()
+                }
+            },
+            b'>' => (), // set numeric keypad mode
+            b'=' => (), // set application keypad mode
+            //b']' => (),
+            _ => ()
+        }
+    }
+
+    fn csi_escape(&mut self) {
+        assert_eq!(self.escape.remove(0), 0x1b);
+        assert_eq!(self.escape.remove(0), b'[');
+        if let Some(final_char) = self.escape.pop() {
+            let arg_str = String::from_utf8_lossy(&self.escape);
+            let mut args = Vec::new();
+            for substring in arg_str.split(';') {
+                if let Ok(n_arg) = substring.parse::<usize>() {
+                    args.push(n_arg);
+                } else if substring.len() == 0 {
+                    // this will happen if e.g. \033[;5H is the escape
+                    // that would mean ypos 1, xpos 5
+                    // or if \033[16;H was given as esc, it means ypos 16, xpos 1
+                    // tho our array is 0-indexed and that will be corrected elsewhere
+                    args.push(1);
+                }
+            }
+            while args.len() < 2 {
+                args.push(1);
+            }
+            match final_char {
+                b'A' => self.cursor_up(args[0]),
+                b'B' => self.cursor_down(args[0]),
+                b'C' => self.cursor_forward(args[0]),
+                b'D' => self.cursor_back(args[0]),
+                b'E' => self.cursor_next_line(args[0]),
+                b'F' => self.cursor_prev_line(args[0]),
+                b'G' => self.cursor_set_column(args[0]),
+                b'd' => self.cursor_set_row(args[0]),
+                // the esc sequence is \033[<line>;<column>H
+                b'H' | b'f' => self.cursor_set_position(args[0], args[1]),
+                b'J' => self.erase_display(args[0]),
+                b'K' => self.erase_inline(args[0]),
+                b'm' => (), // ignore colours
+                _ => ()
+            }
+        }
+    }
+
+    pub fn dump(&self, out: &mut Stderr) {
+        for j in 0 .. self.size.height {
+            for i in 0 .. self.size.width {
+                if self.grid[j][i] == '\0' {
+                    write!(out, " ");
+                } else {
+                    write!(out, "{}", self.grid[j][i]);
+                }
+            }
+            write!(out, "\n");
+        }
+    }
+
+    fn line_feed(&mut self) {
+        self.cursor_down(1);
+    }
+
+    fn carriage_return(&mut self) {
+        self.cursor_set_column(1);
+    }
+
+    fn backspace(&mut self) {
+        if self.cursor.x != 0 {
+            self.cursor_back(1);
+            self.put_char(' ');
+            self.cursor_back(1);
+        }
+    }
+
+    fn cursor_up(&mut self, count: usize) {
+        self.cursor.y -= count;
+    }
+
+    fn cursor_down(&mut self, count: usize) {
+        self.cursor.y += count;
+
+        if self.cursor.y >= self.size.height {
+            self.cursor.y = self.size.height - 1;
+        }
+    }
+
+    fn cursor_forward(&mut self, count: usize) {
+        self.cursor.x += count;
+        if self.cursor.x >= self.size.width {
+            self.cursor.x = self.size.width - 1;
+        }
+    }
+
+    fn cursor_back(&mut self, count: usize) {
+        self.cursor.x -= count;
+    }
+
+    fn cursor_next_line(&mut self, count: usize) {
+        self.cursor.y += count;
+        if self.cursor.y >= self.size.height {
+            self.cursor.y = self.size.height - 1;
+        }
+        self.cursor.x = 0;
+    }
+
+    fn cursor_prev_line(&mut self, count: usize) {
+        self.cursor.y -= count;
+        self.cursor.x = 0;
+    }
+
+    fn cursor_set_column(&mut self, pos: usize) {
+        self.cursor.x = pos - 1;    // terminal indexing starts at 1
+    }
+
+    fn cursor_set_row(&mut self, pos: usize) {
+        self.cursor.y = pos - 1;    // terminal indexing starts at 1
+    }
+
+    fn cursor_set_position(&mut self, ypos: usize, xpos: usize) {
+        self.cursor.x = xpos - 1;    // terminal indexing starts at 1
+        self.cursor.y = ypos - 1;
+
+        if self.cursor.x >= self.size.width {
+            self.cursor.x = self.size.width - 1;
+        }
+        if self.cursor.y >= self.size.height {
+            self.cursor.y = self.size.height - 1;
+        }
+    }
+
+    fn erase_display(&mut self, mode: usize) {
+        match mode {
+            1 => {
+                // clear from cursor to beginning of screen
+                for j in 0 .. self.cursor.y - 1 {
+                    self.clear_line(j);
+                }
+                for i in 0 .. self.cursor.x {
+                    self.grid[self.cursor.y][i] = '\0';
+                }
+            },
+            2 | 3 => {
+                for j in 0 .. self.size.height {
+                    self.clear_line(j);
+                }
+                self.cursor.x = 0;
+                self.cursor.y = 0;
+            },
+            _ => {
+                for i in self.cursor.x .. self.size.width {
+                    self.grid[self.cursor.y][i] = '\0';
+                }
+                for j in self.cursor.y + 1 .. self.size.height {
+                    self.clear_line(j);
+                }
+            }
+        }
+    }
+
+    fn erase_inline(&mut self, mode: usize) {
+        match mode {
+            1 => {
+                for i in 0 .. self.cursor.x {
+                    self.grid[self.cursor.y][i] = '\0';
+                }
+            },
+            2 => {
+                self.clear_line(self.cursor.y);
+            },
+            _ => {
+                for i in self.cursor.x .. self.size.width {
+                    self.grid[self.cursor.y][i] = '\0';
+                }
+            }
+        }
+    }
+
+    fn clear_line(&mut self, line_index: usize) {
+        for i in 0 .. self.size.width {
+            self.grid[line_index][i] = '\0';
+        }
+    }
+
+    fn put_char(&mut self, text: char) {
+        let post_conversion = match self.mapg0 {
+            CharMap::IsoStandard => text,
+            CharMap::VTGraphics => convert_vt(text),
+            _ => text
+        };
+        self.grid[self.cursor.y][self.cursor.x] = post_conversion;
+        self.cursor.x += 1;
+        if self.cursor.x >= self.size.width {
+            self.cursor.y += 1;
+            self.cursor.x = 0;
+        }
+        if self.cursor.y >= self.size.height {
+            // wrapping instead of scrolling could cause issues...
+            self.cursor.y = 0;
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let (ws_col, ws_row) = terminal_size()?;
     let (ws_ypixel, ws_xpixel) = terminal_size_pixels()?;
@@ -43,12 +484,14 @@ fn main() -> Result<()> {
         ws_xpixel,
         ws_ypixel 
     };
+    let mut model = GameScreen::new(ws_col as usize, ws_row as usize);
     let fork = forkpty(Some(&win_size), None)?;
 
     if fork.fork_result.is_parent() {
         let mut stdout = stdout().into_raw_mode().unwrap();
         let stdin = stdin();
-        let mut raw_fd = fork.master;
+        let mut stderr = stderr();
+        let raw_fd = fork.master;
 
         // Read output via PTY master
         //let output = String::new();
@@ -66,7 +509,7 @@ fn main() -> Result<()> {
 
         // spawn a background thread to deal with the input
         
-        let input_handler = thread::spawn(move || {
+        let _input_handler = thread::spawn(move || {
             // loop over events on the term input,(_eventkey, bytevec)
             // forward keys to child process
             for event in stdin.events_and_raw() {
@@ -74,11 +517,12 @@ fn main() -> Result<()> {
                     raw_write(raw_fd, &byte_vector);
                     unistd::fsync(raw_fd);
                 }
-                thread::sleep(Duration::from_millis(100));
+                thread::sleep(Duration::from_millis(50));
             }
         });
 
         // continue reading, and copy raw to our stdout
+        //let mut loop_count = 0;
         loop {
             let mut buffer: [u8; 4096] = [0; 4096];
             match raw_read(raw_fd, &mut buffer) {
@@ -86,6 +530,12 @@ fn main() -> Result<()> {
                     if n == 0 { break }
                     write!(stdout, "{}", String::from_utf8_lossy(&mut buffer[..n]))?;
                     stdout.flush();
+                    //for i in 0 .. n {
+                    //    write!(stderr, "{:#x} ", buffer[i]);
+                    //}
+                    //write!(stderr, "{}", String::from_utf8_lossy(&mut buffer[..n]))?;
+                    //stderr.flush();
+                    model.update(&buffer[..n]);
                 },
                 Err(e) => {
                     //println!("error reading output sent to {}: {}", our_pty.unwrap(), e);
@@ -93,14 +543,22 @@ fn main() -> Result<()> {
                     return Err(Box::new(e));
                 }
             }
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_millis(50));
+            //write!(stderr, "\nabove was raw, below is a dump of the buffer\n");
+            //loop_count += 1;
+            //if loop_count >= 5 {
+            //    model.dump(&mut stderr);
+            //    stderr.flush();
+            //    return Ok(());
+            //}
         }
     } else {
         // Child process just exec `tty`
         //Command::new("tty").status().expect("could not execute tty");
         //Command::new("stty").arg("-a").status().expect("could not execute stty -a");
-        //Command::new("nethack").status().expect("could not execute local nethack");
-        Command::new("ssh").arg("hdf").status().expect("could not execute local nethack"); 
+        Command::new("nethack").status().expect("could not execute local nethack");
+        //Command::new("ssh").arg("hdf").status().expect("could not execute local nethack");
+        //Command::new("sh").status().expect("could not execute shell");
 
     }
     Ok(())
