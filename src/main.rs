06b6291904b33463f36daed8d7ff5760b26bbe22
Joanna Doyle <jjadoyle@gmail.com>
2020-11-20 08:12:20 +0100

    refactor more terminal code into src/term.rs
    Signed-off-by: Joanna Doyle <jjadoyle@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -19,68 +19,60 @@ extern crate nix;
 extern crate termion;
 extern crate terminal_emulator;
 mod term;
-use crate::term::{get_winsize, sizeinfo_from, PtyReader};
+use crate::term::{fork_terminal, TermFork};
 use std::error;
-use std::io::{stdin, stdout};
+use std::io::{stdin, stdout, Write};
 use std::process::Command;
 use std::thread;
 //use std::time::Duration;
-use nix::unistd;
-use nix::pty::forkpty;
 use terminal_emulator::ansi::Processor;
-use terminal_emulator::term::Term;
 use termion::raw::IntoRawMode;
 use termion::input::TermReadEventsAndRaw;
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 fn main() -> Result<()> {
-    let win_size = get_winsize()?;
-    let fork = forkpty(Some(&win_size), None)?;
 
-    if fork.fork_result.is_parent() {
-        let mut stdout = stdout().into_raw_mode().unwrap();
-        let stdin = stdin();
-        let raw_fd = fork.master;
+    match fork_terminal()? {
+        TermFork::Parent(pty_reader, mut pty_writer, mut terminal) => {
+            let mut stdout = stdout().into_raw_mode().unwrap();
+            let stdin = stdin();
+            let mut processor = Processor::new();
 
-        let size_info = sizeinfo_from(win_size);
-        let mut terminal = Term::new(size_info);
-        let mut processor = Processor::new();
-
-        let child_stdout = PtyReader::new(raw_fd);
-
-        // spawn a background thread to deal with the input
-        
-        let _join_handler = thread::spawn(move || {
-            // loop over events on the term input,(_eventkey, bytevec)
-            // forward keys to child process
-            for event in stdin.events_and_raw() {
-                if let Ok((_event, byte_vector)) = event {
-                    unistd::write(raw_fd, &byte_vector);
-                    unistd::fsync(raw_fd);
+            // spawn a background thread to deal with the input
+            
+            let _join_handler = thread::spawn(move || {
+                // loop over events on the term input,(_eventkey, bytevec)
+                // forward keys to child process
+                for event in stdin.events_and_raw() {
+                    if let Ok((_event, byte_vector)) = event {
+                        pty_writer.write(&byte_vector);
+                        pty_writer.flush();                        
+                    }
+                    //thread::sleep(Duration::from_millis(50));
                 }
+            });
+
+            // would like to abstract this raw_read stuff a bit,
+            // and just have a bytes iterator coming in, and being
+            // passed to processor.advance()
+            for c in pty_reader {
+                // do stuff with received byte
+                processor.advance(&mut terminal, c, &mut stdout);
                 //thread::sleep(Duration::from_millis(50));
             }
-        });
 
-        // would like to abstract this raw_read stuff a bit,
-        // and just have a bytes iterator coming in, and being
-        // passed to processor.advance()
-        for c in child_stdout {
-            // do stuff with received byte
-            processor.advance(&mut terminal, c, &mut stdout);
-            //thread::sleep(Duration::from_millis(50));
+            Ok(())
+        },
+        TermFork::Child => {
+            // Child process just exec `tty`
+            Command::new("tty").status().expect("could not execute tty");
+            //Command::new("stty").arg("-a").status().expect("could not execute stty -a");
+            //Command::new("nethack").status().expect("could not execute local nethack");
+            //Command::new("ssh").arg("hdf").status().expect("could not execute local nethack");
+            //Command::new("sh").status().expect("could not execute shell");
+            Ok(())
         }
-
-        Ok(())
-    } else {
-        // Child process just exec `tty`
-        Command::new("tty").status().expect("could not execute tty");
-        //Command::new("stty").arg("-a").status().expect("could not execute stty -a");
-        //Command::new("nethack").status().expect("could not execute local nethack");
-        //Command::new("ssh").arg("hdf").status().expect("could not execute local nethack");
-        //Command::new("sh").status().expect("could not execute shell");
-        Ok(())
     }
 }
 
--- src/term.rs
+++ src/term.rs
@@ -1,11 +1,11 @@
 extern crate termion;
 extern crate nix;
 use std::error;
-use std::io::{Read, Error as ioErr, ErrorKind as ioErrKind, Result as ioResult};
+use std::io::{Read, Error as ioErr, ErrorKind as ioErrKind, Result as ioResult, Write};
 use std::os::unix::io::RawFd;
 use nix::unistd;
-use nix::pty::Winsize;
-use terminal_emulator::term::SizeInfo;
+use nix::pty::{forkpty, Winsize};
+use terminal_emulator::term::{SizeInfo, Term};
 use termion::{terminal_size, terminal_size_pixels};
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
@@ -75,6 +75,35 @@ impl Iterator for PtyReader {
     }
 }
 
+pub struct PtyWriter {
+    fd: RawFd
+}
+
+impl PtyWriter {
+    pub fn new(raw_fd: RawFd) -> Self {
+        PtyWriter {
+            fd: raw_fd
+        }
+    }
+}
+
+impl Write for PtyWriter {
+    fn write(&mut self, buf: &[u8]) -> ioResult<usize> {
+        unistd::write(self.fd, &buf)
+            .map_err(|e| ioErr::new(ioErrKind::Other, e))
+    }
+
+    fn flush(&mut self) -> ioResult<()> {
+        unistd::fsync(self.fd)
+            .map_err(|e| ioErr::new(ioErrKind::Other, e))
+    }
+}
+
+pub enum TermFork {
+    Parent(PtyReader, PtyWriter, Term),
+    Child
+}
+
 pub fn get_winsize() -> Result<Winsize> {
     let (ws_col, ws_row) = terminal_size()?;
     let (ws_xpixel, ws_ypixel) = terminal_size_pixels()?;
@@ -100,5 +129,21 @@ pub fn sizeinfo_from(win_size: Winsize) -> SizeInfo {
         padding_x: 0.0,
         padding_y: 0.0,
         dpr: 90.0
+    }
+}
+
+pub fn fork_terminal() -> Result<TermFork> {
+    let win_size = get_winsize()?;
+    let fork = forkpty(Some(&win_size), None)?;
+
+    if fork.fork_result.is_parent() {
+        let raw_fd = fork.master;
+
+        let size_info = sizeinfo_from(win_size);
+        let emulator = Term::new(size_info);
+
+        Ok(TermFork::Parent(PtyReader::new(raw_fd), PtyWriter::new(raw_fd), emulator))
+    } else {
+        Ok(TermFork::Child)
     }
 }
